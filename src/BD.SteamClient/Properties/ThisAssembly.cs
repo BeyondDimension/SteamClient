d52733090153b12ce735fe50a05746facfa4fc20
Aigio Liu <lysoft.pw@foxmail.com>
2022-11-10 19:06:45 +0800

    .net 7

--- src/BD.SteamClient/Properties/ThisAssembly.cs
+++ src/BD.SteamClient/Properties/ThisAssembly.cs
@@ -17,5 +17,5 @@ static partial class ThisAssembly
     /// </summary>
     public const string AssemblyCompany = "长沙次元超越科技有限公司";
 
-    public const string AssemblyVersion = "1.22.1016.2003";
+    public const string AssemblyVersion = "1.22.11110.1855";
 }
