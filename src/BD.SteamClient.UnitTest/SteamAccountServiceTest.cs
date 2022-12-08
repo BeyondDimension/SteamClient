f809fa62be94c109ddf5033debbeaf27761cb277
Aigio Liu <lysoft.pw@foxmail.com>
2022-12-09 00:58:41 +0800

    v1.22.11209.10042

--- src/BD.SteamClient.UnitTest/SteamAccountServiceTest.cs
+++ src/BD.SteamClient.UnitTest/SteamAccountServiceTest.cs
@@ -1,41 +1,41 @@
 using BD.SteamClient.Models;
-using BD.SteamClient.Services;
+//using BD.SteamClient.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ProjectRecharge;
 
 public sealed class SteamAccountServiceTest
 {
-    IServiceProvider service;
+    //IServiceProvider service;
 
-    ISteamAccountService Client => service.GetRequiredService<ISteamAccountService>();
+    //ISteamAccountService Client => service.GetRequiredService<ISteamAccountService>();
 
     [SetUp]
     public void Setup()
     {
-        var services = new ServiceCollection();
-        services.AddTransient<ISteamAccountService, SteamAccountServiceImpl>();
-        service = services.BuildServiceProvider();
+        //var services = new ServiceCollection();
+        //services.AddTransient<ISteamAccountService, SteamAccountServiceImpl>();
+        //service = services.BuildServiceProvider();
     }
 
-    [Test]
-    public async Task TestDoLoginV2Async()
-    {
-        var loginState = new SteamLoginResponse
-        {
-            Username = "hhhhhh",
-            Password = "hhhhhh",
-        };
-        await Client.DoLoginV2Async(loginState);
-        TestContext.WriteLine(loginState);
-        Assert.True(loginState.Success);
-    }
+    //[Test]
+    //public async Task TestDoLoginV2Async()
+    //{
+    //    //var loginState = new SteamLoginResponse
+    //    //{
+    //    //    Username = "hhhhhh",
+    //    //    Password = "hhhhhh",
+    //    //};
+    //    //await Client.DoLoginV2Async(loginState);
+    //    //TestContext.WriteLine(loginState);
+    //    //Assert.True(loginState.Success);
+    //}
 
-    [Test]
-    public async Task TestGetRSAkeyV2Async()
-    {
-        var data = await Client.GetRSAkeyV2Async("hhhhhh", "hhhhhh");
-        TestContext.WriteLine(data);
-        Assert.True(data.encryptedPassword64 != null);
-    }
+    //[Test]
+    //public async Task TestGetRSAkeyV2Async()
+    //{
+    //    //var data = await Client.GetRSAkeyV2Async("hhhhhh", "hhhhhh");
+    //    //TestContext.WriteLine(data);
+    //    //Assert.True(data.encryptedPassword64 != null);
+    //}
 }
