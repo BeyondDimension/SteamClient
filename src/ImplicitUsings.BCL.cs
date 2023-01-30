fd04e7aae30080db947dd06a5e7b3d149bc2c3f7
Aigio Liu <lysoft.pw@foxmail.com>
2023-01-31 00:22:56 +0800

    v1.23.10130.11830

--- src/ImplicitUsings.BCL.cs
+++ src/ImplicitUsings.BCL.cs
@@ -1,10 +1,15 @@
 // C# 10 定义全局 using
 
+#pragma warning disable IDE0079 // 请删除不必要的忽略
+#pragma warning disable IDE0005
+#pragma warning disable SA1209 // Using alias directives should be placed after other using directives
+#pragma warning disable SA1211 // Using alias directives should be ordered alphabetically by alias name
+
 global using Microsoft.Win32;
 #if !NETFRAMEWORK
 global using System.Collections.Immutable;
+global using System.ComponentModel;
 global using System.ComponentModel.DataAnnotations;
-global using System.ComponentModel.DataAnnotations.Schema;
 #endif
 global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
@@ -24,4 +29,5 @@ global using System.Text;
 global using System.Text.Encodings.Web;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
-global using System.Text.Unicode;
+global using System.Runtime.InteropServices;
+global using System.Runtime.Versioning;
