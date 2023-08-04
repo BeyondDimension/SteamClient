baf291b7d47837ec97a131a2f3ab1784a039de17
john.jiang <572164008@qq.com>
2023-08-04 17:59:20 +0800

    🎨 处理 SA1516 换行空格

--- src/BD.SteamClient/Models/SteamSession.cs
+++ src/BD.SteamClient/Models/SteamSession.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace BD.SteamClient.Models;
+
 public class SteamSession
 {
     public string SteamId { get; set; } = string.Empty;
