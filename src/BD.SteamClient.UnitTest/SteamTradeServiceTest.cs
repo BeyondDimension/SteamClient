0ae8589bac79a8364c7c167b979a8f8f3d5f8f0f
john.jiang <572164008@qq.com>
2023-08-16 17:51:04 +0800

    🎨 fix

--- src/BD.SteamClient.UnitTest/SteamTradeServiceTest.cs
+++ src/BD.SteamClient.UnitTest/SteamTradeServiceTest.cs
@@ -38,21 +38,5 @@ public sealed class SteamTradeServiceTest
     [Test]
     public async Task Run()
     {
-        SteamSession steamSession = new SteamSession();
-        steamSession.SteamId = _steamId;
-        var cookieContainer = new CookieContainer();
-        var uri = new Uri("https://steamcommunity.com/");
-        cookieContainer.Add(uri, new Cookie("sessionid", "bb58a1bd59b38d31b002c1f5"));
-        cookieContainer.Add(uri, new Cookie("steamLoginSecure", "76561199079758512||eyAidHlwIjogIkpXVCIsICJhbGciOiAiRWREU0EiIH0.eyAiaXNzIjogInI6MTQyQV8yMkZEQjZDN183OUFBOSIsICJzdWIiOiAiNzY1NjExOTkwNzk3NTg1MTIiLCAiYXVkIjogWyAid2ViIiBdLCAiZXhwIjogMTY5MTgyMzA3MCwgIm5iZiI6IDE2ODMwOTUwNTQsICJpYXQiOiAxNjkxNzM1MDU0LCAianRpIjogIjE0MkFfMjJGREI2QzdfODU3ODEiLCAib2F0IjogMTY5MTczNTA1MywgInJ0X2V4cCI6IDE2OTQzMjEyNDYsICJwZXIiOiAwLCAiaXBfc3ViamVjdCI6ICIxMTAuNTIuMjE3LjEwOSIsICJpcF9jb25maXJtZXIiOiAiMTEwLjUyLjIxNy4xMDkiIH0.0wbZIYUMOD2As9Igs5ycaZQt5yxrxr4BI_8TkR2AhC6ZwqBnf-VbFBDJs6bSHobgZAUI9xd5UzBql3RgRI2pCw"));
-        steamSession.CookieContainer = cookieContainer;
-        steamSession.HttpClient = new HttpClient(new HttpClientHandler() { UseCookies = true, CookieContainer = cookieContainer });
-
-        var trade_rsp = await Client.GetTradeOfferAsync("1AA37B9ED9A6A46995ABC581E92A8978", "6275665902");
-
-        if (trade_rsp.IsSuccessStatusCode)
-        {
-            var tradeInfo = JsonDocument.Parse(await trade_rsp.Content.ReadAsStringAsync()).RootElement.GetProperty("response").GetProperty("offer").Deserialize<TradeInfo>() ?? null;
-
-        }
     }
 }
