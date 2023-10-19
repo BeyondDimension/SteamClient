c673c8ac979cc9db1b3efac7dffb9752ae2347b2
Mossimo <xflat7@hotmail.com>
2023-10-19 14:05:21 +0800

    🐛 修复 \\ 转义保存问题

--- src/BD.SteamClient/Helpers/VdfHelper.cs
+++ src/BD.SteamClient/Helpers/VdfHelper.cs
@@ -11,18 +11,19 @@ public static class VdfHelper
 {
     const string TAG = nameof(VdfHelper);
 
+    private static KVSerializerOptions options = new KVSerializerOptions
+    {
+        HasEscapeSequences = true,
+    };
+
     /// <summary>
     /// 根据路径读取 Valve Data File 内容
     /// </summary>
     /// <param name="filePath"></param>
     /// <returns></returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static KVObject Read(string filePath, bool isBinary = false, bool isHasEscapeSequences = true)
+    public static KVObject Read(string filePath, bool isBinary = false)
     {
-        var options = new KVSerializerOptions
-        {
-            HasEscapeSequences = isHasEscapeSequences,
-        };
         var kv = KVSerializer.Create(isBinary ? KVSerializationFormat.KeyValues1Binary : KVSerializationFormat.KeyValues1Text);
         var data = kv.Deserialize(IOPath.OpenRead(filePath), options);
         return data;
@@ -36,7 +37,7 @@ public static class VdfHelper
             //不要用 FileMode.OpenOrCreate 文件内容长度不一致会导致结尾内容错误
             using var stream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.ReadWrite | FileShare.Delete);
             var kv = KVSerializer.Create(KVSerializationFormat.KeyValues1Text);
-            kv.Serialize(stream, content);
+            kv.Serialize(stream, content, options);
         }
         catch (Exception e)
         {
